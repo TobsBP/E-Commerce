35b85e0541f6e4dd087053fdbee541e1a60a7f5c
Tobias <tobias.bp2004@gmail.com>
2025-12-02 10:02:44 -0300

    reafactor: get the cookies by a function to be more easy

---
+++ src/lib/auth/cookies.ts
@@ -0,0 +1,11 @@
+'use server'
+
+import { cookies } from 'next/headers'
+
+export async function getAuthToken() {
+	const cookieStore = await cookies()
+	const tokenCookie = cookieStore.get('token')
+	const token = tokenCookie?.value
+
+	return token
+}
